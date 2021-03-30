55e627d5f1c0eb8b968905587f0a768fca9da6b9
HOWISYUNI <blissoooooo@gmail.com>
2021-03-30 18:13:56 +0900

    STUDY : chapter5_구글로그인_테스트
    - application.properties에 include문을 통해 oauth properties 맵핑
    
    - IndexController.java : 로그인 정보 추가
    
    - index.mustache : 로그인 버튼 추가
    
    - SessionUser.java Getter, Serializable 추가
    
    - SecurityConfig.java Matchers 수정

--- src/main/java/com/howisyuni/springboot/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/howisyuni/springboot/config/auth/CustomOAuth2UserService.java
@@ -20,7 +20,6 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @Service
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-
     private final UserRepository userRepository;
     private final HttpSession httpSession;
 
@@ -35,7 +34,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
-
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
