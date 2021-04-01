312fbd98d5227413a5385d608cc3ec6f8737dac3
HOWISYUNI <blissoooooo@gmail.com>
2021-04-01 15:01:20 +0900

    STUDY : chapter5_어노테이션으로 코드개선
    - LoginUser.java : 파라미터 어노테이션 선언
    
    - LoginUserArgumentResolver.java : @LoginUser의 조건, 기대동작 선언. HandlerMethodArgumentResolver 상속
    
    - WebConfig.java : @LoginUser를 스프링 어노테이션으로 등록. HandlerMethodArgumentResolver리스트의 원소로 LoginUserArgumentResolver 등록

--- src/main/java/com/howisyuni/springboot/web/IndexController.java
+++ src/main/java/com/howisyuni/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.howisyuni.springboot.web;
 
+import com.howisyuni.springboot.config.auth.LoginUser;
 import com.howisyuni.springboot.config.auth.dto.SessionUser;
 import com.howisyuni.springboot.service.posts.PostsService;
 import com.howisyuni.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
